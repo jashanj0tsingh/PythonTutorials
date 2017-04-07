4541521e1eb6af9af3736b1af475670cce9facb1
Jashanjot Singh <s_jashan@encs.concordia.ca>
2017-04-07 00:52:43 -0400

    A simple python script to scrape lyrics from genius.com for artist's name.

---
+++ LyricsScraper.py
@@ -0,0 +1,34 @@
+# A simple script to scrape lyrics from the genius.com based on atrtist name.
+
+import re
+import requests
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+mybrowser = webdriver.Chrome("E:\chromedriver.exe") # Browser and path to Web driver you wish to automate your tests cases.
+
+user_input = input("Enter Artist Name = ").replace(" ","+") # User_Input = Artist Name
+base_url = "https://genius.com/search?q="+user_input # Append User_Input to search query
+
+mybrowser.get(base_url) # Open in browser
+file = open('lyrics.txt','a')
+
+while(True): # Reach the bottom of the page.
+    mybrowser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    html = mybrowser.page_source
+    soup = BeautifulSoup(html, "lxml")
+    time.sleep(3)
+
+pattern = re.compile("[\S]+-lyrics$") # Filter http links that end with "lyrics".
+pattern2 = re.compile("\[(.*?)\]") # Remove unnecessary [Intro], [Chorus] etc..
+
+
+for link in soup.find_all('a',href=True):
+        if pattern.match(link['href']):
+            f = requests.get(link['href'])
+            lyricsoup = BeautifulSoup(f.content,"html.parser")
+            lyrics = lyricsoup.find("lyrics").get_text().replace("\n","")
+            lyrics = re.sub(pattern2, "", lyrics)
+            file.write(lyrics+"\n")
+file.close()
