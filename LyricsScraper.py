1a646639b12a1a79f3399d2da880ac194ac40dd9
Jashanjot Singh <jshn.singh1@gmail.com>
2017-04-07 17:36:13 -0400

    Update LyricsScraper.py

--- LyricsScraper.py
+++ LyricsScraper.py
@@ -10,11 +10,9 @@ mybrowser = webdriver.Chrome("E:\chromedriver.exe") # Browser and path to Web dr
 
 user_input = input("Enter Artist Name = ").replace(" ","+") # User_Input = Artist Name
 base_url = "https://genius.com/search?q="+user_input # Append User_Input to search query
-
 mybrowser.get(base_url) # Open in browser
-file = open('lyrics.txt','a')
 
-while(True): # Reach the bottom of the page.
+while(condition): # Reach the bottom of the page.
     mybrowser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     html = mybrowser.page_source
     soup = BeautifulSoup(html, "lxml")
@@ -22,7 +20,7 @@ while(True): # Reach the bottom of the page.
 
 pattern = re.compile("[\S]+-lyrics$") # Filter http links that end with "lyrics".
 pattern2 = re.compile("\[(.*?)\]") # Remove unnecessary text from the lyrics such as [Intro], [Chorus] etc..
-
+file = open('lyrics.txt','a')
 
 for link in soup.find_all('a',href=True):
         if pattern.match(link['href']):
@@ -31,4 +29,5 @@ for link in soup.find_all('a',href=True):
             lyrics = lyricsoup.find("lyrics").get_text().replace("\n","")
             lyrics = re.sub(pattern2, "", lyrics)
             file.write(lyrics+"\n")
+mybrowser.close()
 file.close()
